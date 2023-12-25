233d360b769f7da3620c5010518b38685631ddee
bsaiswarya2816 <bsaiswarya2816@gmail.com>
2023-12-25 18:29:09 +0530

    UpdatedGoldenSun

--- src/org/dra/FireAndBlood.java
+++ src/org/dra/FireAndBlood.java
@@ -7,5 +7,11 @@ public class FireAndBlood {
 	private void harwin() {
 		System.out.println("Harwin");
 	}
+	private void jammie() {
+		System.out.println("jammie");
+	}
+	private void briennie() {
+		System.out.println("briennie");
+	}
 
 }
